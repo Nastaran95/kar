3de5a96be3f0b8d2b7285f5a179f6006b65607d1
masafa <mary.sfard@gmail.com>
2018-08-05 00:23:23 +0430

    header and about us page. revise is necessary.

---
+++ aboutUs.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maryam
+ * Date: 8/4/2018
+ * Time: 7:13 PM
+ */
+include 'header.php'
+?>
+
+
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+    <link rel="stylesheet" href="css/global.css"/>
+    <link rel="stylesheet" href="css/helper.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class ="container grayColor">
+    <div class="row">
+        <br/>
+        <div class="container">
+
+            <h1 class="dark_text">درباره ما</h1>
+            <p class="dark_text">
+                گروه مدیریت تجاری و توسعه کسب و کار مرکز مطالعات تكنولوژي دانشگاه صنعتي شريف (کارآسا) به عنوان بازوی اجرایی مرکز مطالعات دانشگاه صنعتی شریف با بیش از ده سال سابقه مشاوره، آموزش و پژوهش در جهت کمک به تامین، جذب، آموزش و نگه داشت نیروی انسانی سازمان ها، نهادها، صنایع و شرکت ها، خدمات زیر را ارائه می دهد.
+            </p>
+
+            <div class="opening_bars">
+                <div class="bar"> برگزاری آزمون استخدامی</div>
+                <div class="bar">انجام مصاحبه تخصصی</div>
+                <div class="bar">انجام مصاحبه روانشناختی</div>
+                <div class="bar">جذب از طریق رزومه</div>
+            </div>
+
+        </div>
+
+
+
+    </div>
+</div>
+</body>
--- header.php
+++ header.php
@@ -5,3 +5,42 @@
  * Date: 8/2/18
  * Time: 7:54 PM
  */
+?>
+<div class="header col-lg-12 col-md-12">
+<!--    <div class="cover">-->
+<!--        <div class="cover_text">-->
+<!--            <h1 class="title">کاراسا</h1>-->
+<!--            <h2>پل ارتباطی بین کارجو و کارفرما</h2>-->
+<!--        </div>-->
+<!--        <div class="cover_menu menu">-->
+<!--            <ul class="dark_text">-->
+<!--                <li>صفحه نخست</li>-->
+<!--                <li>آزمون های گذشته</li>-->
+<!--                <li>اخبار ویژه</li>-->
+<!--                <li>مشتریان</li>-->
+<!--                <li>بلاگ</li>-->
+<!--                <li>درباره ما</li>-->
+<!--                <li>تماس با ما</li>-->
+<!--            </ul>-->
+<!--            <img id="logo" src="images/logo.png">-->
+<!--        </div>-->
+<!--    </div>-->
+    <div class="fixed">
+        <div class="karasa dark light_text">
+            <h1 class="title">کاراسا</h1>
+            <h2>پل ارتباطی بین کارجو و کارفرما</h2>
+        </div>
+        <div class="menu">
+            <ul class="dark_text">
+                <li>صفحه نخست</li>
+                <li>آزمون های گذشته</li>
+                <li>اخبار ویژه</li>
+                <li>مشتریان</li>
+                <li>بلاگ</li>
+                <li>درباره ما</li>
+                <li>تماس با ما</li>
+            </ul>
+            <img id="logo" src="images/logo.png">
+        </div>
+    </div>
+</div>
