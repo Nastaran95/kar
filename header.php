0d1ab4181700ab35f741d2d99e6cc31956a74d93
masafa <mary.sfard@gmail.com>
2018-08-06 00:05:20 +0430

    main header

--- header.php
+++ header.php
@@ -7,24 +7,27 @@
  */
 ?>
 <div class="header col-lg-12 col-md-12">
-<!--    <div class="cover">-->
-<!--        <div class="cover_text">-->
-<!--            <h1 class="title">کاراسا</h1>-->
-<!--            <h2>پل ارتباطی بین کارجو و کارفرما</h2>-->
-<!--        </div>-->
-<!--        <div class="cover_menu menu">-->
-<!--            <ul class="dark_text">-->
-<!--                <li>صفحه نخست</li>-->
-<!--                <li>آزمون های گذشته</li>-->
-<!--                <li>اخبار ویژه</li>-->
-<!--                <li>مشتریان</li>-->
-<!--                <li>بلاگ</li>-->
-<!--                <li>درباره ما</li>-->
-<!--                <li>تماس با ما</li>-->
-<!--            </ul>-->
-<!--            <img id="logo" src="images/logo.png">-->
-<!--        </div>-->
-<!--    </div>-->
+    <div class="cover hide">
+        <div class="cover_photo">
+            <div class="cover_text">
+                <h1 class="title">کاراسا</h1>
+                <h2>پل ارتباطی بین کارجو و کارفرما</h2>
+                <img src="images/logo.png" class="logo">
+            </div>
+            <div class="cover_arrow"><i class="fa fa-angle-down"></i></div>
+        </div>
+        <div class="cover_menu menu">
+            <ul class="dark_text">
+                <li>صفحه نخست</li>
+                <li>آزمون های گذشته</li>
+                <li>اخبار ویژه</li>
+                <li>مشتریان</li>
+                <li>بلاگ</li>
+                <li>درباره ما</li>
+                <li>تماس با ما</li>
+            </ul>
+        </div>
+    </div>
     <div class="fixed">
         <div class="karasa dark light_text">
             <h1 class="title">کاراسا</h1>
