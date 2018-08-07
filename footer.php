af6e050e168c14796481b7be93be4b494743cab5
Nastaran <nmoradzadehf@gmail.com>
2018-08-07 16:20:00 +0430

    Footer complete

--- footer.php
+++ footer.php
@@ -31,21 +31,28 @@
 
     </div>
     <div class="col-md-12"><hr/></div>
-    <div class="container  text-center col-md-12">
-        <div class="col-md-6">
-            <div class="col-md-5">
-                <div class="col-md-1 pull-left icon col-xs-1"><img src="images/telegram.png" ></div>
-                <div class="col-md-10 ">
-                    <a class="" href="">@karasanews</a>
-                </div>
-            </div>
-            <div class="col-md-7">
+    <div class="container">
 
-            </div>
-       </div>
-        <div class="col-md-6">
+        <div class="col-md-1 pull-left icon col-xs-1">
+            <a class="" href=""><img src="images/telegram.png" ></a>
+        </div>
+        <div class="col-md-5">
+            جهت دریافت آخرین اخبار آزمون استخدامی کارآسا عضو کانال ما شوید.
+        </div>
+
+        <div class="col-md-6 ">
+            <div> برای عضویت در خبرنامه لطفا ایمیل خود را وارد نمایید:</div>
+            <form action="">
+                <br>
+                <div class="col-md-1" ></div>
+                <input class="col-md-2" type="submit" value="ثبت">
+                <div class="col-md-1" ></div>
+                <input class="col-md-8" type="email" name="email" size="30">
+
+            </form>
 
         </div>
 
     </div>
+    <div class="col-md-12 text-center footerDown"> کلیه حقوق مادی و معنوی کاراسا متعلق به مرکز مطالعات دانشگاه صنعتی شریف می باشد </div>
 </div>
