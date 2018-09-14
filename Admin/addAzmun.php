a58351d65458955618e84edca44c387e202e43b4
Nastaran <nmoradzadehf@gmail.com>
2018-09-14 19:36:18 +0430

    server changes :)

--- Admin/addAzmun.php
+++ Admin/addAzmun.php
@@ -25,6 +25,7 @@ if ($_SESSION['type']>8) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>افزودن آزمون</title>
 
+        <link rel="stylesheet" href="../css/oldcss.css">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
         <link rel="stylesheet" type="text/css" href="css/local.css" />
@@ -142,7 +143,7 @@ if ($_SESSION['type']>8) {
         $file = $writer->outputMemory();
         file_put_contents($filename, $file);
         $topic = $_POST['topic'];
-//        date_default_timezone_set("Iran");
+        date_default_timezone_set("Iran");
         $modified_time=date('Y-m-d H:i:s');
         // date_default_timezone_set('Asia/Tehran');
 
