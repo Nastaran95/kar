3decfae4c0c0d8ae64347fdfc135e91fa0e50471
Nastaran <nmoradzadehf@gmail.com>
2018-09-17 16:20:31 +0430

    server changes :)

--- Admin/allCustomer.php
+++ Admin/allCustomer.php
@@ -26,7 +26,7 @@ if (($_SESSION['type']>0)) {
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>همه خبر ها</title>
+        <title>همه مشتریان</title>
 
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
